2cf726f4f1f362b2b09ea97133ba7cae60005556
Camila Mello <milla_mello@hotmail.com>
2023-01-24 15:55:00 -0300

    Commit de produto realizado

--- cypress/e2e/pre.cadastro.cy.js
+++ cypress/e2e/pre.cadastro.cy.js
@@ -1,4 +1,4 @@
-// <reference types="cypress" />
+/// <reference types="cypress" />
 var faker = require('faker');
 
 describe('Funcionalidade Pré Cadatro', () => {
@@ -8,13 +8,17 @@ describe('Funcionalidade Pré Cadatro', () => {
         })
 
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
+        var emailfaker = faker.internet.email()
+        var nomeFaker = faker.name.firstName()
+        var sobrenomeFaker = faker.name.lastName()
+        
+        cy.get('#reg_email').type(emailfaker)
         cy.get('#reg_password').type('@Teste2026')
         cy.get(':nth-child(4) > .button').click ()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click ()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso')
---
+++ cypress/e2e/produto.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de produtos', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]')
+            //.first() - primeiro produto da lista
+            //.last - ultimo produto da lista
+            //.eq(3) - 4 produto da lista começando pela contagem 0,1,2,3
+            .contains('Abominable Hoodie')
+            .click()
+
+    });
+    it.only('Deve selecionar um produto par o Carrinho', () => {
+        var quantidade = 2
+        cy.get('[class="product-block grid"]')
+       
+            .contains('Abominable Hoodie').click()
+            cy.get('.button-variable-item-M').click()
+            cy.get('.button-variable-item-Blue').click()
+            cy.get('.input-text').clear().type(quantidade)
+            cy.get('.single_add_to_cart_button').click()
+            
+            cy.get('#cart > .dropdown-toggle').should('contain' , quantidade)
+            cy.get('.woocommerce-message').should('contain' , quantidade + ' × “Abominable Hoodie” foram adicionados no seu carrinho.')
+
+    })
+
+});
+
+
+
+
