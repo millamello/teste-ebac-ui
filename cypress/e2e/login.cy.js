c985961e40e769e09716e64bb21c7a3f7eea1142
Camila Mello <milla_mello@hotmail.com>
2023-01-30 16:20:13 -0300

    Atualização

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -23,13 +23,21 @@ cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
 
 })  
 
-it.only('Deve fazer login com sucesso - Usando arquivo de dados ', () => {
+it('Deve fazer login com sucesso - Usando arquivo de dados ', () => {
     cy.get('#username').type(perfil.usuario)
     cy.get('#password').type(perfil.senha)
     cy.get('.woocommerce-form > .button').click ()
 });
 
+it.only('Deve fazer login com sucesso - Usando Fixture' , () =>{
+cy.fixture('perfil').then(dados => {
+    cy.get('#username').type(dados.usuario)
+    cy.get('#password').type(dados.senha)
+    cy.get('.woocommerce-form > .button').click ()
 
+})
+
+})
 it(' Deve exibir uma mensagem de erro ao inserir senha inválido' , ()=>{
     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
@@ -37,6 +45,8 @@ it(' Deve exibir uma mensagem de erro ao inserir senha inválido' , ()=>{
         cy.get('.woocommerce-form > .button').click ()
         cy.get('.woocommerce-error').should('contain' , 'Erro')
     })
+
+
 
 
 })
